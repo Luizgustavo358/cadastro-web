c93a39038baffdc97a1d216e23f9ff55716491ee
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2019-10-01 18:21:04 -0300

    movendo para pasta de frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Cadastro de Livros</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
